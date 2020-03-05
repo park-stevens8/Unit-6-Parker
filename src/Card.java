a8ca851d77fd1f1a94e5ca63aa17128babfb2b46
park-stevens8 <54951025+park-stevens8@users.noreply.github.com>
2020-03-05 09:29:38 -0800

    Add files via upload

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card{
+  public static final String[] RANK = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"}
+
+  public static final String[] SUIT = {""};
+
+  public int compareTo(){
+
+  }
+
+  public String toString(){
+    
+  }
+}
