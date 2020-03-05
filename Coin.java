aec3b7aacbd2bdce4634173f29f42fa0f46c2ab9
park-stevens8 <54951025+park-stevens8@users.noreply.github.com>
2020-03-05 09:47:47 -0800

    Add files via upload

---
+++ Coin.java
@@ -0,0 +1,28 @@
+public class Coin {
+
+private double myValue;
+private String myName;
+
+public Coin(double value, String name) {
+myValue = value;
+myName = name;
+}
+
+public double getValue() {
+return myValue;
+}
+
+public String getName() {
+return myName;
+}
+
+// returns true if this coin is equal to aCoin,
+// which means the name and the value are both equal
+public boolean equals(Coin aCoin) {
+//COMPLETE THIS METHOD!
+if(this.Coin == aCoin){
+  return true;
+}
+}
+
+}
